2066d2ecd7a8289f3f70abaab0ed14e4a9bb39f5
Big fir tree <weed500feat885@yahoo.co.jp>
2019-10-27 14:48:16 +0000

    Finish user edit, update, index, and destroy actions

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,7 +11,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
-    assert_select 'form[action="/signup"]'
+    assert_select 'form[action="/users"]'
     assert_select 'div#error_explanation'
     assert_select 'div.alert'
     assert_select 'div.alert-danger'
