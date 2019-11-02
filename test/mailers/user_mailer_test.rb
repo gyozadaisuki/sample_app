b37bc38bcaf9de16cd7133b1a93854d01d5aa266
Big fir tree <weed500feat885@yahoo.co.jp>
2019-11-02 07:12:10 +0000

    Add password reset

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -12,5 +12,23 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match user.activation_token,   mail.body.encoded
     assert_match CGI.escape(user.email),  mail.body.encoded
   end
-
+  
+  test "password_reset" do
+    # ユーザーを設定する
+    user = users(:michael)
+    # リセット用トークンを設定する(本来ならば、create_reset_digest内で行われているがメールを送らないのでdigest保存もしない関係でtokenの設定のみ)
+    user.reset_token = User.new_token
+    # リセットメール送信
+    mail = UserMailer.password_reset(user)
+    # メール件名がPassword reset
+    assert_equal "Password reset", mail.subject
+    # ユーザーのメールアドレスに送られているか
+    assert_equal [user.email], mail.to
+    # "noreply@example.com"から送信されているか
+    assert_equal ["noreply@example.com"], mail.from
+    # メール本文にリセット用トークンが入っているか
+    assert_match user.reset_token,        mail.body.encoded
+    # CGIエスケープされたユーザーのメールアドレスが入っているか
+    assert_match CGI.escape(user.email),  mail.body.encoded
+  end
 end
