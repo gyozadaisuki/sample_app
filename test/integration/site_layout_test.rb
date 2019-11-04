20d92ce6cddeef3c74b92f7c2dcc32896861a035
Big fir tree <weed500feat885@yahoo.co.jp>
2019-11-04 14:11:03 +0000

    Add user following

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -20,4 +20,11 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_select "title", full_title("Sign up")
   end
+  
+  test "count relationships" do
+    log_in_as(@user)
+    get root_path
+    assert_match @user.active_relationships.count.to_s, response.body
+    assert_match @user.passive_relationships.count.to_s, response.body
+  end
 end
